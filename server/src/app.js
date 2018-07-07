6de5b9e90db22ff5a62dac849ef36627de8e265a
Kriangkrai Prommaithong <kr.prommaithong@gmail.com>
2018-07-07 10:34:58 +0700

    setup app.js

---
+++ server/src/app.js
@@ -0,0 +1,7 @@
+
+const session = require('express-session')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const app = require('express')()
+const signale = require('signale')
+const passport = require('passport')
