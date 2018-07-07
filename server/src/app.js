ed8f7ffe63639e51bd61f159f2cdc03820537ce3
Kriangkrai Prommaithong <kr.prommaithong@gmail.com>
2018-07-07 10:38:07 +0700

    clear directory

--- server/src/app.js
+++
@@ -1,7 +0,0 @@
-
-const session = require('express-session')
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-const app = require('express')()
-const signale = require('signale')
-const passport = require('passport')
